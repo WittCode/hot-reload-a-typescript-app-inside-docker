59f202a979edbc6e01755a0f05c8b59780f60206
wittcode <wittcode@gmail.com>
2023-11-29 11:28:36 +0000

    hot reload a typescript app inside docker

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import cheese from "./cheese.js";
+
+const sayHello = (name: string) => {
+  console.log(`Hello asdfasdfthere ${name}!`);
+  console.log(cheese)
+};
+
+sayHello('WittCode of justice and tranquility');
